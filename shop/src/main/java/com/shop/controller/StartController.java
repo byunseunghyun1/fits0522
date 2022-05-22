e51801c5a6f1ad0a4577edee1d9b2a8a63b2fb36
YEON <57996603+hhi9920@users.noreply.github.com>
2022-05-22 17:29:21 +0900

    Initial commit

---
+++ shop/src/main/java/com/shop/controller/StartController.java
@@ -0,0 +1,10 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+    @GetMapping("/start")
+    public String mPage(){return "startFits";}
+}
